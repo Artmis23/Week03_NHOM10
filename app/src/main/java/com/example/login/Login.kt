269e72777e343810cd2cb37d87f32d0cf744240c
HuuAn1803 <huuan1803@gmail.com>
2022-03-25 23:24:06 +0700

    huuan

--- app/src/main/java/com/example/login/Login.kt
+++ app/src/main/java/com/example/login/Login.kt
@@ -9,37 +9,43 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.login.SignUp
+import com.example.login.databinding.ActivityLoginBinding
 import com.example.login.viewmodel.MainViewModel
 
 class Login : AppCompatActivity() {
 
     //MVVM
     val mainViewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityLoginBinding
+    //private var edtEmail: EditText? = null
+    //private var edtPassWord: EditText? = null
+    //private var btnLogin: TextView? = null
+    //private var txtSignUp: TextView? = null
 
-    private var edtEmail: EditText? = null
-    private var edtPassWord: EditText? = null
-    private var btnLogin: TextView? = null
-    private var txtSignUp: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        edtEmail = findViewById<View>(R.id.edtEmail) as EditText
-        edtPassWord = findViewById<View>(R.id.edtPassWord) as EditText
-        btnLogin = findViewById<View>(R.id.btnLogin) as TextView
-        txtSignUp = findViewById<View>(R.id.txtSignUp) as TextView
 
-        btnLogin?.setOnClickListener(View.OnClickListener {
+        //setContentView(R.layout.activity_login)
+        //edtEmail = findViewById<View>(R.id.edtEmail) as EditText
+        //edtPassWord = findViewById<View>(R.id.edtPassWord) as EditText
+        //btnLogin = findViewById<View>(R.id.btnLogin) as TextView
+        //txtSignUp = findViewById<View>(R.id.txtSignUp) as TextView
 
-            val email = edtEmail?.text.toString().trim()
-            val password = edtPassWord?.text.toString().trim()
+        binding=DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.btnLogin?.setOnClickListener(View.OnClickListener {
+
+            val email = binding.edtEmail?.text.toString().trim()
+            val password = binding.edtPassWord?.text.toString().trim()
             Log.d("iii", email)
             Log.d("iii", password)
 
             mainViewModel.LoginUser(email, password)
         })
 
-        txtSignUp?.setOnClickListener(View.OnClickListener {
+        binding.txtSignUp?.setOnClickListener(View.OnClickListener {
             val intent2 = Intent(this@Login, SignUp::class.java)
             intent2.flags = (Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
                     or Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -54,8 +60,8 @@ class Login : AppCompatActivity() {
         mainViewModel.isSuccessEvent.observe(this) { isSuccess ->
             if (isSuccess) {
 
-                val email = edtEmail?.text.toString().trim()
-                val password = edtPassWord?.text.toString().trim()
+                val email = binding.edtEmail?.text.toString().trim()
+                val password = binding.edtPassWord?.text.toString().trim()
                 Log.d("iii", email)
                 Log.d("iii", password)
 
